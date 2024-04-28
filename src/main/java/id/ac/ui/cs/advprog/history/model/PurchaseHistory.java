d1ca229839d44fc9a3d4c276761a8b194425033e
facesofgoblin <buatkuliahrana@gmail.com>
2024-04-28 14:02:46 +0700

    Trying to implement factory method pattern but still unsuccessful

--- src/main/java/id/ac/ui/cs/advprog/history/model/PurchaseHistory.java
+++ src/main/java/id/ac/ui/cs/advprog/history/model/PurchaseHistory.java
@@ -1,8 +1,10 @@
 package id.ac.ui.cs.advprog.history.model;
 
+
 import java.util.Date;
 import java.util.List;
 import lombok.Getter;
+
 
 @Getter
 public class PurchaseHistory {
@@ -12,48 +14,13 @@ public class PurchaseHistory {
     private final List<PurchaseItem> purchaseItems;
     private final Long userId;
 
-    private PurchaseHistory(Builder builder) {
-        this.id = builder.id;
-        this.totalCost = builder.totalCost;
-        this.purchaseDate = builder.purchaseDate;
-        this.purchaseItems = builder.purchaseItems;
-        this.userId = builder.userId;
-    }
 
-    public static class Builder {
-        private Long id;
-        private Double totalCost;
-        private Date purchaseDate;
-        private List<PurchaseItem> purchaseItems;
-        private Long userId;
-
-        public Builder withId(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public Builder withTotalCost(Double totalCost) {
-            this.totalCost = totalCost;
-            return this;
-        }
-
-        public Builder withPurchaseDate(Date purchaseDate) {
-            this.purchaseDate = purchaseDate;
-            return this;
-        }
-
-        public Builder withPurchaseItems(List<PurchaseItem> purchaseItems) {
-            this.purchaseItems = purchaseItems;
-            return this;
-        }
-
-        public Builder withUserId(Long userId) {
-            this.userId = userId;
-            return this;
-        }
-
-        public PurchaseHistory build() {
-            return new PurchaseHistory(this);
-        }
+    // Konstruktor langsung tanpa Builder
+    public PurchaseHistory(Long id, Double totalCost, Date purchaseDate, List<PurchaseItem> purchaseItems, Long userId) {
+        this.id = id;
+        this.totalCost = totalCost;
+        this.purchaseDate = purchaseDate;
+        this.purchaseItems = purchaseItems;
+        this.userId = userId;
     }
 }
