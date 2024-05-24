aad6efefae7648616ad9cfcd4779b1a7cb6df50e
facesofgoblin <buatkuliahrana@gmail.com>
2024-05-25 02:22:35 +0700

    JWT Setup

--- src/main/java/id/ac/ui/cs/advprog/history/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/history/model/User.java
@@ -1,12 +1,53 @@
 package id.ac.ui.cs.advprog.history.model;
+
+import java.util.Collection;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//gaperlu kelas ini karena nanti dihubungkan sama punya gilang
-@Getter
-@Setter
-public class User {
-    private Long id;
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class User implements UserDetails {
+    private UUID id;
     private String username;
+    private String password;
+    private String email;
+    private String role;
+    private Collection<? extends GrantedAuthority> authorities;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
